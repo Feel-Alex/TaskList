50c1b0832060d7441a2b5fa226e07096cf9e5d02
Feel-Alex <85347739+Feel-Alex@users.noreply.github.com>
2021-12-15 18:15:16 +0300

    The layouts is ready

--- app/src/main/java/com/presintation/MainActivity.kt
+++ app/src/main/java/com/presintation/MainActivity.kt
@@ -5,14 +5,18 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.example.tasklist.R
+import com.example.tasklist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var viewModel: MainViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.taskList.observe(this){
             Log.d("My", it.toString())
